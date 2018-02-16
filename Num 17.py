d1161e301b7c4ce735abf9a85a1533b18a903231
yohza37 <Yohanesa001@e.ntu.edu.sg>
2018-02-17 01:07:09 +0700

    Final
    Test case 3 4 5 somehow is wrong, but work perfectly in most cases.

--- Num 17.py
+++ Num 17.py
@@ -1,291 +1,139 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> dicta = {}
->>> dicta[1] = dicta[1] + 1
-Traceback (most recent call last):
-  File "<pyshell#1>", line 1, in <module>
-    dicta[1] = dicta[1] + 1
-KeyError: 1
->>> dicta[1] = 3
->>> dicta.keys
-<built-in method keys of dict object at 0x0505FF00>
->>> dicta.keys()
-dict_keys([1])
->>> print(dicta.keys())
-dict_keys([1])
->>> dicta
-{1: 3}
->>> dicta['ganteng'] = 3
->>> dicta
-{1: 3, 'ganteng': 3}
->>> dicta.keys()
-dict_keys([1, 'ganteng'])
->>> ganteng in dicta.keys()
-Traceback (most recent call last):
-  File "<pyshell#10>", line 1, in <module>
-    ganteng in dicta.keys()
-NameError: name 'ganteng' is not defined
->>> 'ganteng' in dicta.keys
-Traceback (most recent call last):
-  File "<pyshell#11>", line 1, in <module>
-    'ganteng' in dicta.keys
-TypeError: argument of type 'builtin_function_or_method' is not iterable
->>> 
->>> print(ganteng in dicta.keys())
-Traceback (most recent call last):
-  File "<pyshell#13>", line 1, in <module>
-    print(ganteng in dicta.keys())
-NameError: name 'ganteng' is not defined
->>> print("ganteng" in dicta.keys())
-True
->>> dictb = dicta
->>> dictb
-{1: 3, 'ganteng': 3}
->>> dicta['cibe
-	  
-SyntaxError: EOL while scanning string literal
->>> dicta['cap
-	  
-SyntaxError: EOL while scanning string literal
->>> dicta[3] = [4,5]
-	  
->>> dicta 3
-	  
-SyntaxError: invalid syntax
->>> dicta[3]
-	  
-[4, 5]
->>> n = 10
-	  
->>> n3 = 10//3
-	  
->>> n3
-	  
-3
->>> n5 = 10 // 5
-	  
->>> n = 10
-	  
->>> n = n-1
-	  
->>> n3 = 10//3
-	  
->>> n5 = 10 // 5
-	  
->>> print(n5)
-	  
-2
->>> n3= n//3
-	  
->>> n5 = n//5
-	  
->>> n3
-	  
-3
->>> n5
-	  
-1
->>> n15 = n//15
-	  
->>> n15
-	  
-0
->>> n3 = n3*(3+n3*3)/2
-	  
->>> n3
-	  
-18.0
->>> n5 = n5*(5+n5*5)/2
-	  
->>> n5
-	  
-5.0
->>> n3+n5
-	  
-23.0
->>> a = " "
-	  
->>> a = " "
-	  
->>> a == " "
-	  
-True
->>> a = [1,2,3,4,5]
-	  
->>> a.pop(4)
-	  
-5
->>> a
-	  
-[1, 2, 3, 4]
->>> a = [1,2,3]
-	  
->>> b = [1,2,3]
-	  
->>> b.pop(0)
-	  
-1
->>> b = a
-	  
->>> b
-	  
-[1, 2, 3]
->>> from itertools import combinations
-	  
->>> comb = combinations([1,3,4],3)
-	  
->>> com
-	  
-Traceback (most recent call last):
-  File "<pyshell#55>", line 1, in <module>
-    com
-NameError: name 'com' is not defined
->>> comb
-	  
-<itertools.combinations object at 0x054A5900>
->>> print(comb)
-	  
-<itertools.combinations object at 0x054A5900>
->>> comb[1]
-	  
-Traceback (most recent call last):
-  File "<pyshell#58>", line 1, in <module>
-    comb[1]
-TypeError: 'itertools.combinations' object is not subscriptable
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-Traceback (most recent call last):
-  File "C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py", line 19, in <module>
-    print(summa([1,2,3,4,5],6))
-  File "C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py", line 4, in summa
-    for i in range(len(a)) :
-TypeError: object of type 'int' has no len()
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-Traceback (most recent call last):
-  File "C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py", line 19, in <module>
-    print(summa([1,2,3,4,5],6))
-  File "C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py", line 7, in summa
-    if not(v in dicta.key()) :
-AttributeError: 'dict' object has no attribute 'key'
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-None
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-4
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-2
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
-5
-5
-5
-5
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-{1: 1, 2: 1, 3: 1, 4: 1, 5: 1}
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
-5
-5
-5
-5
-2
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-1
-2
-3
-4
-5
-2
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-2
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
-4
-3
-2
-1
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
-1
-4
-2
-3
-3
-2
-4
-1
-5
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-1
-2
-3
-4
-5
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-3
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-5
-4
-3
-2
-1
-5
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-4
->>> 
-= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python36-32/Golden.py =
-2
->>> a
-	  
-Traceback (most recent call last):
-  File "<pyshell#59>", line 1, in <module>
-    a
-NameError: name 'a' is not defined
->>> "a
-	  
-SyntaxError: EOL while scanning string literal
->>> "a"
-	  
-'a'
->>> "a" + "b"
-	  
-'ab'
->>> 
+def UndThousand(n) :
+    Hun = n // 100
+    Ten = n % 100
+    ends = " " + "Hundred" + " " + UndHundred(Ten)
+    if Hun == 1 :
+        return "One" + ends
+    if Hun == 2 :
+        return "Two" + ends
+    if Hun == 3 :
+        return "Three" + ends
+    if Hun == 4 :
+        return "Four" + ends
+    if Hun == 5 :
+        return "Five" + ends
+    if Hun == 6 :
+        return "Six" + ends
+    if Hun == 7 :
+        return "Seven" + ends
+    if Hun == 8 :
+        return "Eight" + ends
+    if Hun == 9 :
+        return "Nine" + ends
+    return UndHundred(n)
+
+def splitz(n) :
+    out = []
+    while True :
+        out.insert(0,n%1000)
+        n = n // 1000
+        if n == 0 :
+            return out
+        
+def NumtoWords(n) :
+    arr = splitz(n)
+    final = ""
+    for i in range(len(arr)) :
+        ps = arr[len(arr)-1-i]
+        pros = UndThousand(ps)
+        if i == 0 :
+            if ps == 0 :
+                continue
+            final = pros + final
+        if i == 1 :
+            if ps == 0 :
+                continue
+            final = pros + " " + "Thousand" + " " + final
+        if i == 2 :
+            if ps == 0 :
+                continue
+            else :
+                final = pros + " " + "Million" + " " + final
+        if i == 3 :
+            if ps == 0 :
+                continue
+            else :
+                final = pros + " " + "Billion" + " " + final
+        if i == 4 :
+            if ps == 0 :
+                continue
+            else :
+                final = pros + " " + "Trillion" + " " + final
+        if i == 5 :
+            if ps == 0 :
+                continue
+            else :
+                final = pros + " " + "Quadrillion" + " " + final
+    return final
+
+def UndHundred(n) :
+    if n == 10 :
+        return "Ten"
+    if n == 11 :
+        return "Eleven"
+    if n == 12 :
+        return "Twelve"
+    if n == 13 :
+        return "Thirteen"
+    if n == 14 :
+        return "Fourteen"
+    if n == 15 :
+        return "Fifteen"
+    if n == 16 :
+        return "Sixteen"
+    if n == 17 :
+        return "Seventeen"
+    if n == 18 :
+        return "Eighteen"
+    if n == 19 :
+        return "Nineteen"
+    Tenth = n // 10
+    if Tenth == 2 :
+        return "Twenty" + " " + UndTen(n % 10)
+    if Tenth == 3 :
+        return "Thirty" + " " + UndTen(n % 10)
+    if Tenth == 4 :
+        return "Forty" + " " + UndTen(n%10)
+    if Tenth == 5 :
+        return "Fifty" + " " + UndTen(n%10)
+    if Tenth == 6 :
+        return "Sixty" + " " + UndTen(n%10)
+    if Tenth == 7 :
+        return "Seventy" + " " + UndTen(n%10)
+    if Tenth == 8 :
+        return "Eighty"  + " " + UndTen(n%10)
+    if Tenth == 9 :
+        return "Ninety" + " " + UndTen(n%10)
+    return UndTen(n)
+    
+def UndTen(n) :
+    if n == 0 :
+        return ""
+    if n == 1 :
+        return "One"
+    if n == 2 :
+        return "Two"
+    if n == 3 :
+        return "Three"
+    if n == 4 :
+        return "Four"
+    if n == 5 :
+        return "Five"
+    if n == 6 :
+        return "Six"
+    if n == 7 :
+        return "Seven"
+    if n == 8 :
+        return "Eight"
+    if n == 9 :
+        return "Nine"
+    
+k = int(input())
+
+for i in range(k) :
+    c = int(input())
+    if c == 0 :
+        print("Zero")
+    else :
+        print(NumtoWords(c))
+
